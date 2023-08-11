6b033f8463f240c79a5d1acb874fadc19076c244
MaxyZ <maximoleonel83@gmail.com>
2023-08-11 00:41:55 -0300

    00-html y script agregado, base completo

---
+++ script.js
@@ -0,0 +1,11 @@
+//funcion sumar
+
+//funcion IMC = peso (kg)/[estatura (m)]*2
+
+//funcion dar vuelta una palabra
+
+//funcion calculadora
+
+//funcion control de alcholemia
+
+//funcion multa por exceso de velocidad
